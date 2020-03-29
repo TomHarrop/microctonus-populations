00afc2a2894675879d78c8e7b87ca27baf615f70
Tom Harrop <twharrop@gmail.com>
2020-03-30 09:38:01 +1300

    initial commit

---
+++ Snakefile
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import multiprocessing
+import pandas
+
+###########
+# GLOBALS #
+###########
+
+hyp_ref = 'data/mhyp.fa'
+aeth_ref = 'data/maeth.fa'
+sample_csv = 'data/samples.csv'
+
+# software
+honeybee_genotype_pipeline = (
+    'shub://TomHarrop/'
+    'honeybee-genotype-pipeline:honeybee_genotype_pipeline_v0.0.7')
+samtools = 'shub://TomHarrop/singularity-containers:samtools_1.9'
+
+########
+# MAIN #
+########
+
+# get a list of samples
+sample_data = pandas.read_csv(sample_csv,
+                              index_col='sample')
+all_samples = sorted(set(sample_data.index))
+
+#########
+# RULES #
+#########
+
+rule target:
+	input:
+		expand('output/010_genotypes/{ref}/calls.vcf.gz',
+			   ref=['hyp', 'aeth'])
+
+checkpoint genotype:
+    input:
+        csv = sample_csv,
+        ref = lambda wildcards: hyp_ref if wildcards.ref == 'hyp' else aeth_ref
+    output:
+        cutoffs = 'output/010_genotypes/{ref}/040_stats/ldepth.mean_cutoffs.csv',
+        vcf = 'output/010_genotypes/{ref}/calls.vcf.gz',
+        bam = 'output/010_genotypes/{ref}/merged.bam',
+        ref = 'output/010_genotypes/{ref}/015_ref/ref.fasta',
+        fai = 'output/010_genotypes/{ref}/015_ref/ref.fasta.fai',
+    params:
+        wd = 'output/010_genotypes/{ref}',
+        ploidy = '2'
+    log:
+        'output/logs/genotype.{ref}.log'
+    threads:
+        multiprocessing.cpu_count()
+    singularity:
+        honeybee_genotype_pipeline
+    shell:
+        'honeybee_genotype_pipeline '
+        '--ref {input.ref} '
+        '--samples_csv {input.csv} '
+        '--outdir {params.wd} '
+        '--ploidy {params.ploidy} '
+        '--threads {threads} '
+        '--restart_times 1 '
+        '&> {log}'
---
+++ setup/setup_samples.R
@@ -0,0 +1,67 @@
+#!/usr/bin/env Rscript
+
+library(data.table)
+library(seqinr)
+
+RevComp <- function(x){
+    toupper(c2s(rev(comp(s2c(x)))))
+}
+
+
+sample_sheet_raw <- fread("setup/SampleSheet_MiSeq_E7600.csv", skip = 21)
+all_indivs <- fread("setup/gdna_to_libprep.csv")
+final_pool <- fread("setup/microctonus_lib_290120.csv")
+
+# mung the barcodes in sample_sheet
+munged_barcodes <- rbind(
+    sample_sheet_raw[, .(index_id = I7_Index_ID,
+                         index = index)],
+    unique(sample_sheet_raw[, .(index_id = I5_Index_ID,
+                                index = RevComp(index2)),
+                            by = index2][, .(index_id, index)]))
+
+# set up barcode sequences
+samples_only <- final_pool[, .(
+    sample = sample_id,
+    i7_primer,
+    i5_primer)]
+
+
+samples_i7 <- merge(samples_only,
+                    munged_barcodes[, .(index_id, i7_index = index)],
+                    by.x = "i7_primer", by.y = "index_id")
+samples_both <- merge(samples_i7,
+                      munged_barcodes[, .(index_id, i5_index = index)],
+                      by.x = "i5_primer", by.y = "index_id")
+samples_both[, barcode_sequence := paste(i7_index, i5_index, sep = "+")]
+
+# set up metadata
+merged_data <- merge(samples_both,
+                     all_indivs[, .(
+                         sample = indiv_name,
+                         host_name = host_name,
+                         para_name = para_name,
+                         location = location
+                     )],
+                     by = "sample",
+                     all.x = TRUE,
+                     all.y = FALSE)
+
+sample_table <- merged_data[, .(
+    sample = paste(
+        sample,
+        host_name,
+        sub("_", "", para_name),
+        location,
+        sep = "_"),
+    barcode = barcode_sequence,
+    r1_path = paste0("data/reads/", sample, "/", sample, "_R1.fq.gz"),
+    r2_path = paste0("data/reads/", sample, "/", sample, "_R2.fq.gz"),
+    metadata = paste(
+        host_name,
+        sub("_", "", para_name),
+        location,
+        sep = "_")
+)]
+
+fwrite(sample_table, "data/samples.csv")
