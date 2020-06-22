780af6d8da3057072ec6683aea988a66ab0ade68
Tom Harrop <twharrop@gmail.com>
2020-06-22 14:58:40 +1200

    demux

--- src/generate_sample_csv.py
+++ src/generate_sample_csv.py
@@ -2,14 +2,15 @@
 
 import pandas
 from pathlib import Path
+import snakemake as sm
 
-print(snakemake)
+print(snakemake.input)
 
 sample_data = pandas.read_csv(snakemake.input['sample_csv'],
                               index_col='sample')
 
 cdir = snakemake.input['directory']
-my_bcs = glob_wildcards(Path(cdir, '{bc}_r1.fastq.gz')).bc
+my_bcs = sm.io.glob_wildcards(Path(cdir, '{bc}_r1.fastq.gz')).bc
 my_mask = sample_data['barcode'].isin(my_bcs)
 my_csv = sample_data[my_mask]
 my_csv['r1_path'] = [f'output/000_tmp/reads/{x}_r1.fastq.gz'
